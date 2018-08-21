75073369ad48b6bd202145167453d471f67b9346
Hemalathaaaa <42492551+Hemalathaaaa@users.noreply.github.com>
2018-08-21 22:04:25 +0530

    first-21

--- proj_2/proj_2/settings.py
+++ proj_2/proj_2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%a_j_yxi%djfvhcad#xjiknly(^q+1*)0xhps3uyb%l=msb90w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hemalatha/pythonanywhere.com']
 
 
 # Application definition
